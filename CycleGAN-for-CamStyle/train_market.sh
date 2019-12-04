aad749d3df2a6de49b455680a7ea0577d8cbb876
Glen Brown <gbrown29@qub.ac.uk>
2019-12-04 14:54:09 +0000

    Dockerfile for training cyclegans for Market. (#1)
    Both local debugging and docker enabled.

--- CycleGAN-for-CamStyle/train_market.sh
+++ CycleGAN-for-CamStyle/train_market.sh
@@ -1,21 +1,36 @@
 # train all cameras for market
 
-python train.py --dataroot ./datasets/market --name market-c1-c2 --camA 1 --camB 2
-python train.py --dataroot ./datasets/market --name market-c1-c3 --camA 1 --camB 3
-python train.py --dataroot ./datasets/market --name market-c1-c4 --camA 1 --camB 4
-python train.py --dataroot ./datasets/market --name market-c1-c5 --camA 1 --camB 5
-python train.py --dataroot ./datasets/market --name market-c1-c6 --camA 1 --camB 6
+if [ -z "${ROOT_DATASET_DIR}" ]; then 
+    ROOT_DATASET_DIR=~/datasets
+else 
+    ROOT_DATASET_DIR=${ROOT_DATASET_DIR}
+fi
 
-python train.py --dataroot ./datasets/market --name market-c2-c3 --camA 2 --camB 3
-python train.py --dataroot ./datasets/market --name market-c2-c4 --camA 2 --camB 4
-python train.py --dataroot ./datasets/market --name market-c2-c5 --camA 2 --camB 5
-python train.py --dataroot ./datasets/market --name market-c2-c6 --camA 2 --camB 6
+if [ -z "${RESULTS_DIR}" ]; then 
+    RESULTS_DIR=~/results
+else 
+    RESULTS_DIR=${RESULTS_DIR}
+fi
 
-python train.py --dataroot ./datasets/market --name market-c3-c4 --camA 3 --camB 4
-python train.py --dataroot ./datasets/market --name market-c3-c5 --camA 3 --camB 5
-python train.py --dataroot ./datasets/market --name market-c3-c6 --camA 3 --camB 6
+echo $ROOT_DATASET_DIR
+echo $RESULTS_DIR
 
-python train.py --dataroot ./datasets/market --name market-c4-c5 --camA 4 --camB 5
-python train.py --dataroot ./datasets/market --name market-c4-c6 --camA 4 --camB 6
+python train.py --dataroot $ROOT_DATASET_DIR/market --name market-c1-c2 --camA 1 --camB 2 --checkpoints_dir $RESULTS_DIR/camstyle_market_cyclegans/
+python train.py --dataroot $ROOT_DATASET_DIR/market --name market-c1-c3 --camA 1 --camB 3 --checkpoints_dir $RESULTS_DIR/camstyle_market_cyclegans/
+python train.py --dataroot $ROOT_DATASET_DIR/market --name market-c1-c4 --camA 1 --camB 4 --checkpoints_dir $RESULTS_DIR/camstyle_market_cyclegans/
+python train.py --dataroot $ROOT_DATASET_DIR/market --name market-c1-c5 --camA 1 --camB 5 --checkpoints_dir $RESULTS_DIR/camstyle_market_cyclegans/
+python train.py --dataroot $ROOT_DATASET_DIR/market --name market-c1-c6 --camA 1 --camB 6 --checkpoints_dir $RESULTS_DIR/camstyle_market_cyclegans/
 
-python train.py --dataroot ./datasets/market --name market-c5-c6 --camA 5 --camB 6
+python train.py --dataroot $ROOT_DATASET_DIR/market --name market-c2-c3 --camA 2 --camB 3 --checkpoints_dir $RESULTS_DIR/camstyle_market_cyclegans/
+python train.py --dataroot $ROOT_DATASET_DIR/market --name market-c2-c4 --camA 2 --camB 4 --checkpoints_dir $RESULTS_DIR/camstyle_market_cyclegans/
+python train.py --dataroot $ROOT_DATASET_DIR/market --name market-c2-c5 --camA 2 --camB 5 --checkpoints_dir $RESULTS_DIR/camstyle_market_cyclegans/
+python train.py --dataroot $ROOT_DATASET_DIR/market --name market-c2-c6 --camA 2 --camB 6 --checkpoints_dir $RESULTS_DIR/camstyle_market_cyclegans/
+
+python train.py --dataroot $ROOT_DATASET_DIR/market --name market-c3-c4 --camA 3 --camB 4 --checkpoints_dir $RESULTS_DIR/camstyle_market_cyclegans/
+python train.py --dataroot $ROOT_DATASET_DIR/market --name market-c3-c5 --camA 3 --camB 5 --checkpoints_dir $RESULTS_DIR/camstyle_market_cyclegans/
+python train.py --dataroot $ROOT_DATASET_DIR/market --name market-c3-c6 --camA 3 --camB 6 --checkpoints_dir $RESULTS_DIR/camstyle_market_cyclegans/
+
+python train.py --dataroot $ROOT_DATASET_DIR/market --name market-c4-c5 --camA 4 --camB 5 --checkpoints_dir $RESULTS_DIR/camstyle_market_cyclegans/
+python train.py --dataroot $ROOT_DATASET_DIR/market --name market-c4-c6 --camA 4 --camB 6 --checkpoints_dir $RESULTS_DIR/camstyle_market_cyclegans/
+
+python train.py --dataroot $ROOT_DATASET_DIR/market --name market-c5-c6 --camA 5 --camB 6 --checkpoints_dir $RESULTS_DIR/camstyle_market_cyclegans/
---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM pytorch/pytorch:0.4.1-cuda9-cudnn7-devel
+
+# Make the directories in root.
+RUN mkdir /src
+
+# Copy requirements file to the src dir.
+WORKDIR /src
+COPY CycleGAN-for-CamStyle/requirements.txt /src
+
+# Install pip modules
+RUN pip install --upgrade pip && pip install -r requirements.txt
+
+# Copy the rest of the code.
+COPY . /src
+
+# Set docker env variables.
+ENV ROOT_DATASET_DIR=/datasets
+ENV RESULTS_DIR=/results
+
+# Default to training the market.
+WORKDIR /src/CycleGAN-for-CamStyle
+CMD ./train_market.sh
