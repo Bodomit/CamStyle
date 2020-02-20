e957a693ab75edd522b4401203a635454de1715f
Glen Brown <40057686@login4.pri.kelvin2.alces.network>
2020-02-20 19:50:26 +0000

    Longer running time.

--- sbatch_jobs/vggface2pose_synth/gen/vggface2pose_synth-gen-XY.sh
+++ sbatch_jobs/vggface2pose_synth/gen/vggface2pose_synth-gen-XY.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=4
 #SBATCH --mem-per-cpu=16G
 #SBATCH --gres gpu:1
-#SBATCH --time=3:00:00
+#SBATCH --time=24:00:00
 #SBATCH --output=/users/40057686/sharedscratch/logs/CAMStyle-TrainVGGFace2PoseSynthMMF-%j.log
 
 module add nvidia-cuda
