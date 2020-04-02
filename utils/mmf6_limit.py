741819da654d53e17e738070b411fed03ff523e7
Glen Brown <gbrown29@qub.ac.uk>
2020-04-02 17:56:46 +0100

    Mmf indecam 10 (#8)
    * Utility script to constrain dataset to n images per id per cam.
    
    * Scripts to train mmf6-indicam-10 cyclegans.
    
    * Choosing random images to keep: not just first k
    
    * Import cleanup.
    
    * Fixed path.
    
    Co-authored-by: Glen Brown <40057686@login1.pri.kelvin2.alces.network>

---
+++ utils/mmf6_limit.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import os
+import glob
+import argparse
+import random
+
+from itertools import groupby
+from typing import Set
+
+
+def main(directory: str, n: int):
+
+    image_paths = os.path.join(directory, "**/*.png")
+    all_images = set(glob.glob(image_paths, recursive=True))
+    images_to_keep : Set[str] = set()
+
+    for _, g in groupby(sorted(all_images), key=lambda x:os.path.dirname(x)):
+        images_to_keep.update(random.choices(list(g), k=10))
+
+    images_to_delete = all_images - images_to_keep
+
+    print("Total Images: {}".format(len(all_images)))
+    print("Images to keep: {}".format(len(images_to_keep)))
+    print("Paths to delete: {}".format(len(images_to_delete)))
+
+    for path in images_to_delete:
+        os.remove(path)
+
+    print("Complete!")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("directory", metavar="DIR")
+    parser.add_argument("--num", "-n", metavar="INT", default=10)
+    args = parser.parse_args()
+    main(args.directory, int(args.num))
